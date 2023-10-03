3497c6e32e5d2c4491d25c2acc94f8b9fb15fa16
Victor Collasanta <victor.collasanta@gmail.com>
2023-10-03 13:24:53 -0300

    status bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,18 +25,20 @@ export default function RootLayout({
   return (
     <ClerkProvider localization={ptBR}>
       <html lang="en">
-          <meta name="apple-mobile-web-app-capable" content="yes"></meta>
-          <link href="../public/splashscreens/iphone5_splash.png" sizes='320x568'  rel="apple-touch-startup-image" />
-          <link href="../public/splashscreens/iphone6_splash.png" sizes='375x667' rel="apple-touch-startup-image" />
-          <link href="../public/splashscreens/iphoneplus_splash.png" sizes='621x1104'  rel="apple-touch-startup-image" />
-          <link href="../public/splashscreens/iphonex_splash.png" sizes='375x812'  rel="apple-touch-startup-image" />
-          <link href="../public/splashscreens/iphonexr_splash.png" sizes='414x896' rel="apple-touch-startup-image" />
-          <link href="../public/splashscreens/iphonexsmax_splash.png" sizes='414x869' rel="apple-touch-startup-image" />
-          <link href="../public/splashscreens/ipad_splash.png" sizes='768x1024'  rel="apple-touch-startup-image" />
-          <link href="../public/splashscreens/ipadpro1_splash.png" sizes='834x1112' rel="apple-touch-startup-image" />
-          <link href="../public/splashscreens/ipadpro3_splash.png" sizes='834x1194'  rel="apple-touch-startup-image" />
-          <link href="../public/splashscreens/ipadpro2_splash.png" sizes='1024x1366' rel="apple-touch-startup-image" />
-          {/* <link href="../public/splashscreens/iphone5_splash.png" sizes='320x568' media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover, width=device-width"></meta>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+        <link href="../public/splashscreens/iphone5_splash.png" sizes='320x568' rel="apple-touch-startup-image" />
+        <link href="../public/splashscreens/iphone6_splash.png" sizes='375x667' rel="apple-touch-startup-image" />
+        <link href="../public/splashscreens/iphoneplus_splash.png" sizes='621x1104' rel="apple-touch-startup-image" />
+        <link href="../public/splashscreens/iphonex_splash.png" sizes='375x812' rel="apple-touch-startup-image" />
+        <link href="../public/splashscreens/iphonexr_splash.png" sizes='414x896' rel="apple-touch-startup-image" />
+        <link href="../public/splashscreens/iphonexsmax_splash.png" sizes='414x869' rel="apple-touch-startup-image" />
+        <link href="../public/splashscreens/ipad_splash.png" sizes='768x1024' rel="apple-touch-startup-image" />
+        <link href="../public/splashscreens/ipadpro1_splash.png" sizes='834x1112' rel="apple-touch-startup-image" />
+        <link href="../public/splashscreens/ipadpro3_splash.png" sizes='834x1194' rel="apple-touch-startup-image" />
+        <link href="../public/splashscreens/ipadpro2_splash.png" sizes='1024x1366' rel="apple-touch-startup-image" />
+        {/* <link href="../public/splashscreens/iphone5_splash.png" sizes='320x568' media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
           <link href="../public/splashscreens/iphone6_splash.png" sizes='375x667' media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
           <link href="../public/splashscreens/iphoneplus_splash.png" sizes='621x1104' media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
           <link href="../public/splashscreens/iphonex_splash.png" sizes='375x812' media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
