df56390f4cd5b9167842dfb1638ff4416a33fc88
Victor Collasanta <victor.collasanta@gmail.com>
2023-10-03 16:20:37 -0300

    apple splash

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,23 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Acompanha.app',
   description: 'Sua Ficha de Acompanhamento Online',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'acompanha.app',
+    startupImage: [
+      { media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)', url: '/iphone5_splash.png' },
+      { media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)', url: '/iphone6_splash.png' },
+      { media: '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)', url: '/iphoneplus_splash.png' },
+      { media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)', url: '/iphonex_splash.png' },
+      { media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)', url: '/iphonexr_splash.png' },
+      { media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)', url: '/iphonexsmax_splash.png' },
+      { media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)', url: '/ipadpro1_splash.png' },
+      { media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)', url: '/ipadpro3_splash.png' },
+      { media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)', url: '/ipadpro2_splash.png' },
+    ],
+  },
+  themeColor: '#f1f5f9',
 }
 
 export default function RootLayout({
@@ -25,13 +42,13 @@ export default function RootLayout({
   return (
     <ClerkProvider localization={ptBR}>
       <html lang="en">
-        <meta name="apple-mobile-web-app-title" content="acompanha.app" />
-        <meta name="apple-touch-fullscreen" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default"></meta>
-        <meta name="theme-color" content="#f1f5f9"></meta>
-        <link href="iphone5_splash.png" sizes='320x568' media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        {/* <meta name="apple-mobile-web-app-title" content="acompanha.app" />
+        <meta name="apple-touch-fullscreen" content="yes" /> */}
+        {/* <meta name="apple-mobile-web-app-capable" content="yes" /> */}
+        {/* <meta name="mobile-web-app-capable" content="yes" /> */}
+        {/* <meta name="apple-mobile-web-app-status-bar-style" content="default"></meta>
+        <meta name="theme-color" content="#f1f5f9"></meta> */}
+        {/* <link href="iphone5_splash.png" sizes='320x568' media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
         <link href="iphone6_splash.png" sizes='375x667' media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
         <link href="iphoneplus_splash.png" sizes='621x1104' media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
         <link href="iphonex_splash.png" sizes='375x812' media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
@@ -39,7 +56,7 @@ export default function RootLayout({
         <link href="iphonexsmax_splash.png" sizes='414x869' media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
         <link href="ipadpro1_splash.png" sizes='834x1112' media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
         <link href="ipadpro3_splash.png" sizes='834x1194' media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
-        <link href="ipadpro2_splash.png" sizes='1024x1366' media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+        <link href="ipadpro2_splash.png" sizes='1024x1366' media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" /> */}
         <GoogleAnalytics GA_MEASUREMENT_ID={process.env.GA_MEASUREMENT_ID!} />
         {/* <CrispProvider /> */}
         <body className={inter.className}>
