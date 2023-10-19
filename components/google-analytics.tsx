eb6ca3e05937c72308864c5677120a8d0b1e4090
Victor Collasanta <victor.collasanta@gmail.com>
2023-10-19 20:39:47 -0300

    test

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -10,7 +10,7 @@ export default function GoogleAnalytics({GA_MEASUREMENT_ID} : {GA_MEASUREMENT_ID
   const searchParams = useSearchParams()
 
   useEffect(() => {
-      const url = pathname + searchParams.toString()
+      const url = pathname + searchParams?.toString()!
       pageview(GA_MEASUREMENT_ID, url);
   }, [pathname, searchParams, GA_MEASUREMENT_ID]);  
 
