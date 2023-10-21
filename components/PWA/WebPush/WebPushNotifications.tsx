5ff6faa0c51b1860716993cd997315a97edace12
Victor Collasanta <victor.collasanta@gmail.com>
2023-10-21 00:14:40 -0300

    change button css

--- components/PWA/WebPush/WebPushNotifications.tsx
+++ components/PWA/WebPush/WebPushNotifications.tsx
@@ -100,7 +100,7 @@ export default function Notifications({ programId }: { programId: string }) {
           </AlertDialogHeader>
           <AlertDialogFooter>
             {/* <AlertDialogAction className="mx-20"> */}
-            <button className='mx-20 py-2 bg-primary text-primary-foreground hover:bg-[#059669] inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-5' onClick={subscribeButtonOnClick}>
+            <button className='h-[55px] bg-primary text-primary-foreground rounded-md text-sm font-medium ' onClick={subscribeButtonOnClick}>
               Ativar Notificações
             </button>
             {/* </AlertDialogAction> */}
