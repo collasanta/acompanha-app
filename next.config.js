b075c3fd3eab3ce656d8bbc8439c5411c938981b
Victor Collasanta <victor.collasanta@gmail.com>
2023-10-21 01:43:12 -0300

    skipWaiting true

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  workboxOptions: {
+    skipWaiting: true,
+  }
   // buildExcludes: [/\/_next\/static\/.*\.js/],
   // reloadOnOnline: true,
 });
