35144a40336966b8b0e37cb9a665796203b5c068
Victor Collasanta <victor.collasanta@gmail.com>
2023-10-20 23:22:03 -0300

    exclude caching

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
+  buildExcludes: [/\/_next\/static\/.*\.js/],
+  reloadOnOnline: true,
 });
 
 /** @type {import("next").NextConfig} */
