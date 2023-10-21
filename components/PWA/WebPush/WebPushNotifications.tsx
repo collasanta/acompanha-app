aa9be42eb30125e67c8efe14e80a96423bab1ea0
Victor Collasanta <victor.collasanta@gmail.com>
2023-10-20 23:53:21 -0300

    try to fix subscribebutton remove awaits

--- components/PWA/WebPush/WebPushNotifications.tsx
+++ components/PWA/WebPush/WebPushNotifications.tsx
@@ -6,6 +6,19 @@ import {
 } from "@/lib/pwa"
 import { MouseEventHandler, useEffect, useState } from "react"
 
+const base64ToUint8Array = (base64: string) => {
+  const padding = "=".repeat((4 - (base64.length % 4)) % 4);
+  const b64 = (base64 + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(b64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+};
+
 export default function Notifications({ programId }: { programId: string }) {
   const [open, setOpen] = useState<boolean>(false)
   const [registration, setRegistration] = useState<ServiceWorkerRegistration | null>(null);
@@ -47,26 +60,22 @@ export default function Notifications({ programId }: { programId: string }) {
     }
   }, [])
 
-  const base64ToUint8Array = async (base64: string) => {
-    const padding = "=".repeat((4 - (base64.length % 4)) % 4);
-    const b64 = (base64 + padding).replace(/-/g, "+").replace(/_/g, "/");
-  
-    const rawData = window.atob(b64);
-    const outputArray = new Uint8Array(rawData.length);
-  
-    for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-  };
+
 
   const subscribeButtonOnClick: MouseEventHandler<HTMLButtonElement> = async (
     event
   ) => {
+    if (!process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY) {
+      throw new Error("Environment variables supplied not sufficient.");
+    }
+    if (!registration) {
+      console.error("No SW registration available.");
+      return;
+    }
     event.preventDefault();
-    const sub = await registration?.pushManager.subscribe({
+    const sub = await registration.pushManager.subscribe({
       userVisibleOnly: true,
-      applicationServerKey: await base64ToUint8Array(
+      applicationServerKey: base64ToUint8Array(
         process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY!
       ),
     });
